f10321174736c3866747a71dfe0053f343279174
Odd Stråbø <oddstr13@openshell.no>
2024-01-14 15:48:33 +0100

    Update to Django 1.8

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 
-urlpatterns = patterns('pastebin.views',
-    url(r'^$', 'index'),
-    url(r'^create/?$', 'create'),
-    url(r'^(?P<urlid>.+)/$', 'show'),
-    url(r'^(?P<urlid>.+)/raw/?$', 'showraw'),
-    url(r'^(?P<urlid>.+)/reply/?$', 'reply'),
-)
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^create/?$', views.create),
+    url(r'^(?P<urlid>.+)/$', views.show),
+    url(r'^(?P<urlid>.+)/raw/?$', views.showraw),
+    url(r'^(?P<urlid>.+)/reply/?$', views.reply),
+]
